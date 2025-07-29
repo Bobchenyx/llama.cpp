92b60a57244dc5fcb4863f1741ad6996d0582201
Yixiao Chen <113320753+Bobchenyx@users.noreply.github.com>
2025-07-29 15:37:27 -0400

    Force Q8_0 for Unsupported

--- src/llama-quant.cpp
+++ src/llama-quant.cpp
@@ -659,8 +659,8 @@ static ggml_type llama_tensor_get_type(quantize_state_impl & qs, ggml_type new_t
             case GGML_TYPE_Q2_K:
             case GGML_TYPE_Q3_K:
             case GGML_TYPE_IQ4_XS: new_type = GGML_TYPE_Q8_0;   break;
-            case GGML_TYPE_Q4_K:   new_type = GGML_TYPE_Q5_0;   break;
-            case GGML_TYPE_Q5_K:   new_type = GGML_TYPE_Q5_1;   break;
+            case GGML_TYPE_Q4_K:   new_type = GGML_TYPE_Q8_0;   break;
+            case GGML_TYPE_Q5_K:   new_type = GGML_TYPE_Q8_0;   break;
             case GGML_TYPE_Q6_K:   new_type = GGML_TYPE_Q8_0;   break;
             default: throw std::runtime_error("\nUnsupported tensor size encountered\n");
         }
